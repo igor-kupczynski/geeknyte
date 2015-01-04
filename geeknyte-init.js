28e0a7f98082a22bd1f84add2229c1a1fc415fe0
Igor Kupczyński <igor@kupczynski.info>
2015-01-04 14:29:26 +0100

    Initial commit - basic support for ls put get

---
+++ geeknyte-init.js
@@ -0,0 +1,31 @@
+'use strict';
+const fs = require('fs'),
+      prompt = require('prompt'),
+      schema = {
+          properties: {
+              username: {
+                  required: true
+              },
+              password: {
+                  required: true,
+                  hidden: true
+              },
+              "domain": {
+                  required: true
+              },
+              "api_key": {
+                  required: true
+              }
+          }
+      };
+prompt.message = '';
+prompt.delimiter = ':';
+prompt.start();
+prompt.get(schema, function (err, result) {
+    if (err) {
+        throw err;
+    }
+    let path = process.env.HOME + '/.egnyte-auth.json';
+    fs.writeFileSync(path, JSON.stringify(result));
+    console.log("File " + path + " updated");
+});
---
+++ geeknyte.js
@@ -0,0 +1,14 @@
+'use strict';
+const user_action = process.argv[2] || "",
+      user_action_args = process.argv.slice(3) || [],
+      egn = require('./lib/egn')(),
+      op = require('./lib/op.js')(egn.egnyte_js_sdk),
+      action = op.actions[user_action];
+
+if (!action) {
+    throw "Action is required, e.g. $ node --harmony geeknyte.js ls /Shared";
+}
+
+egn.authenticated()
+    .then(action.apply(this, user_action_args))
+    .done();
